0c466bcac17e88f44e90e5b85750afa5465598c9
khashab2 <khashab2@illinois.edu>
2015-11-18 20:58:07 -0600

    adding script to generate output of lbj files

---
+++ lbjava-examples/compileLBJ.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+function generateLBJFiles() {
+    if [ $# -eq 0 ];
+    then
+        echo "Usage: compileLBJ <lbj file>"
+    fi
+
+    FILE=$*
+    JAVA=java
+    JAVAC=javac
+
+    JAVA="nice "$JAVA
+    SWITCHES="-ea -XX:MaxPermSize=1g -Xmx8g"
+
+    BIN=target
+    LBJBIN=target
+    SRC=src/main/java
+    GSP=src/main/java
+
+    CP=$BIN:$LBJBIN:lib/*:../lib/liblinear.jar:../lbjava/target/LBJava-1.2.0.jar
+
+    echo $CP
+
+    $JAVA $SWITCHES -cp $CP edu.illinois.cs.cogcomp.lbjava.Main -d $LBJBIN -gsp $GSP -sourcepath $SRC $FILE
+}
+
+function generateAllLBJFiles() {
+  declare -a files=("BadgesClassifier.lbj" "EntityRelation.lbj" "NewsGroupClassifier.lbj" "SentimentClassifier.lbj" "SetCover.lbj" "SpamClassifier.lbj")
+
+  for i in "${files[@]}"
+  do
+     echo "Generating output for $i"
+     generateLBJFiles "src/main/lbj/"$i
+  done
+}
+
+# running starts here
+generateAllLBJFiles
