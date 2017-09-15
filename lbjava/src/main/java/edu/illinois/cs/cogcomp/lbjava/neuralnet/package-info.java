af2ed1c0eb93d8ad6144e9054f615386ca4ec2df
Thomas L. Redman <redman@ncsa.uiuc.edu>
2017-09-15 14:45:08 -0500

    Added the license headers and moved the NNBrownClassifier to examples. Updates the poms to 1.3.1.

--- lbjava/src/main/java/edu/illinois/cs/cogcomp/lbjava/neuralnet/package-info.java
+++ lbjava/src/main/java/edu/illinois/cs/cogcomp/lbjava/neuralnet/package-info.java
@@ -1,5 +1,12 @@
 /**
- * This package contains the Neural Network implemented employed by LBJava. This
+ * This software is released under the University of Illinois/Research and Academic Use License. See
+ * the LICENSE file in the root folder for details. Copyright (c) 2016
+ *
+ * Developed by: The Cognitive Computations Group, University of Illinois at Urbana-Champaign
+ * http://cogcomp.cs.illinois.edu/
+ */
+/**
+ *  This package contains the Neural Network implemented employed by LBJava. This
  * implementation supports bias, momentum and back prop, and is designed with
  * efficiency in mind. The implementation contract includes an API for trainers
  * {@see NNTrainingInterface} that defines the API for the any trainers. A single
